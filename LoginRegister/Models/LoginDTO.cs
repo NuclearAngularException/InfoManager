d2372c98f84bca9af610c08150334f6302768489
chemi600 <132613028+chemi600@users.noreply.github.com>
2025-02-13 20:22:18 +0100

    Proyecto Login/Register

---
+++ LoginRegister/Models/LoginDTO.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace InfoManager.Models
+{
+    public class LoginDTO 
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ LoginRegister/View/DashboardView.xaml.cs
@@ -0,0 +1,18 @@
+using InfoManager.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
+
+
+namespace InfoManager.View;
+public partial class DashboardView : UserControl
+{
+    public DashboardView()
+    {
+        InitializeComponent();
+    }
+
+    private void MyDataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+    {
+        App.Current.Services.GetService<DashboardViewModel>().MyDataGrid_CellEditEnding(sender, e);
+    }
+}

b1aa88fc3a77bb7ab75373c77e691cbb60600b94
Huguinii <hugomnbn@gmail.com>
2025-03-06 15:32:00 +0100

    Pull

--- ProAPI/Migrations/20250306020032_Initial.cs
+++ ProAPI/Migrations/20250306020032_Initial.cs
@@ -5,10 +5,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace RestAPI.Migrations
 {
-    /// <inheritdoc />
     public partial class Initial : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(

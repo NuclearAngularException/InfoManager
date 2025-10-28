bb798d56f94aff3d07cc23c25db1ac3ae086fe0e
Ethan <ethaanbarru@gmail.com>
2025-10-28 15:30:16 +0100

    Cambiar el footer a componente

--- AngularFront/src/app/app.component.ts
+++ AngularFront/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-
+import { FooterComponent } from './component/footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, FooterComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
